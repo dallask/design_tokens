d0babde6ea12069e7a35023d503250b243b2133f
Ievgen_Kyvgyla <Ievgen_Kyvgyla@epam.com>
2023-02-07 14:13:15 +0200

    DF-29346 | Added token-transformer and style-dictionary based on the https://github.com/emulsify-ds/emulsify-ui-kit

---
+++ tokensTransform.js
@@ -0,0 +1,63 @@
+const StyleDictionary =
+  require('style-dictionary').extend('config.json');
+
+const pixelsToRem = (px) => {
+  const rem = 16;
+  return `${px / rem}rem`;
+};
+
+const appendPX = (value) => {
+  return `${value}px`;
+};
+
+StyleDictionary.registerTransform({
+  name: 'toRem/pxToRem',
+  type: 'value',
+  matcher(token) {
+    return (
+      (token.type === 'dimension' ||
+      token.type === 'fontSizes' ||
+      token.type === 'paragraphSpacing' ||
+      token.type === 'sizing' ||
+      token.type === 'spacing' ||
+      token.type === 'borderRadius' ||
+      token.path[0] === 'breakpoint' ||
+      token.path[0] === 'fontSize') && token.attributes.type !== 'multi-value'
+    );
+  },
+  transformer(token) {
+    return pixelsToRem(token.value);
+  },
+});
+
+StyleDictionary.registerTransform({
+  name: 'toRem/appendRem',
+  type: 'value',
+  matcher(token) {
+    return (
+        token.attributes.type === 'multi-value'
+    );
+  },
+  transformer(token) {
+    let a = token.value.split(' ');
+
+    let b = a.map(a => (
+        pixelsToRem(a)
+    ));
+
+    return b.join(' ');
+  },
+});
+
+StyleDictionary.registerTransform({
+  name: 'toPX/appendPX',
+  type: 'value',
+  matcher(token) {
+    return token.type === 'borderWidth';
+  },
+  transformer(token) {
+    return appendPX(token.value);
+  },
+});
+
+StyleDictionary.buildAllPlatforms();
